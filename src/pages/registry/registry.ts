7f905dff8733caddd69cc07df49e261be68b32a8
mikhailbys <bystrovmikhailad@gmail.com>
2021-10-28 00:14:01 +0400

    начало работы с валидацией на странице регистрации

--- src/pages/registry/registry.ts
+++ src/pages/registry/registry.ts
@@ -1,4 +1,60 @@
+import {triggerValidateError} from "../../utils/FormValidation";
+import {PASSWORD_REGEXP} from "../../utils/Regexps";
+
 const registryForm: HTMLFormElement | null = document.querySelector('#registry_form');
+
+function validate() {
+    let result = true;
+
+    // select inputs
+    const login = registryForm?.login;
+    const password = registryForm?.password;
+    const passwordRepeat = registryForm?.passwordRepeat;
+    const mail = registryForm?.mail;
+    const firstName = registryForm?.firstName;
+    const phone = registryForm?.phone;
+    const secondName = registryForm?.secondName;
+
+    // hide input errors
+    password.classList.remove('input-error');
+    login.classList.remove('input-error');
+    passwordRepeat.classList.remove('input-error');
+    mail.classList.remove('input-error');
+    firstName.classList.remove('input-error');
+    secondName.classList.remove('input-error');
+    phone.classList.remove('input-error');
+
+    // hide clues
+    document.querySelector('#login-empty')?.classList.remove('show-error');
+    document.querySelector('#password-empty')?.classList.remove('show-error');
+    document.querySelector('#password-error')?.classList.remove('show-error');
+    document.querySelector('#phone-empty')?.classList.remove('show-error');
+    document.querySelector('#first-name-empty')?.classList.remove('show-error');
+    document.querySelector('#second-name-empty')?.classList.remove('show-error');
+    document.querySelector('#mail-empty')?.classList.remove('show-error');
+
+    if (login.value === '') {
+        triggerValidateError(login, '#login-empty');
+        result = false;
+    }
+
+    if (password.value === '') {
+        triggerValidateError(password, '#password-empty');
+        result = false;
+    }
+
+    if (passwordRepeat.value === '') {
+        triggerValidateError(passwordRepeat, '#password-repeat-empty');
+        result = false;
+    }
+
+    if (password.value !== '' && !password.value.match(PASSWORD_REGEXP)) {
+        triggerValidateError(password, '#password-error');
+        result = false;
+    }
+
+    return result;
+}
 
 if (registryForm) {
     registryForm.onsubmit = (e) => {
@@ -12,7 +68,7 @@ if (registryForm) {
         const password = formData.get('password');
         const passwordRepeat = formData.get('password-repeat');
 
-        console.log({
+        validate() ? console.log({
             login,
             password,
             passwordRepeat,
@@ -20,7 +76,7 @@ if (registryForm) {
             firstName,
             secondName,
             phone,
-        });
+        }) : null;
 
         e.preventDefault();
     };
