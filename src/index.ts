6f3fcabcef98944b407087ff26e6c59dbbcef796
mikhailbys <bystrovmikhailad@gmail.com>
2021-10-27 23:37:02 +0400

    валидация на странице авторизации

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,55 @@
+import {PASSWORD_REGEXP} from "./utils/Regexps";
+import {triggerValidateError} from "./utils/FormValidation";
+
 const authForm: HTMLFormElement | null = document.querySelector('#auth_form');
 
-if (authForm) {
-    authForm.onsubmit = (e) => {
+function validate() {
+    let result = true;
+
+    // select inputs
+    const login = authForm?.login;
+    const password = authForm?.password;
+
+    // hide input errors
+    password.classList.remove('input-error');
+    login.classList.remove('input-error');
+
+    // hide clues
+    document.querySelector('#login-empty')?.classList.remove('show-error');
+    document.querySelector('#password-empty')?.classList.remove('show-error');
+    document.querySelector('#password-error')?.classList.remove('show-error');
+
+    if (login.value === '') {
+        triggerValidateError(login, '#login-empty');
+        result = false;
+    }
+
+    if (password.value === '') {
+        triggerValidateError(password, '#password-empty');
+        result = false;
+    }
+
+    if (password.value !== '' && !password.value.match(PASSWORD_REGEXP)) {
+        triggerValidateError(password, '#password-error');
+        result = false;
+    }
+
+    return result;
+}
+
+function onSubmit(event: Event) {
+    if(authForm) {
         const formData = new FormData(authForm);
 
         const login = formData.get('login');
         const password = formData.get('password');
 
-        console.log({ login, password });
+        event.preventDefault();
 
-        e.preventDefault();
-    };
+        validate() ? console.log({login, password}) : null;
+    }
+}
+
+if (authForm) {
+    authForm.onsubmit = onSubmit;
 }
---
+++ src/utils/Regexps.ts
@@ -0,0 +1,1 @@
+export const PASSWORD_REGEXP = '/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/';
