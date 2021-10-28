a481d1674feb2f8dd5e74c4ec7c1f27b90c650ee
mikhailbys <bystrovmikhailad@gmail.com>
2021-10-28 22:43:11 +0400

    валидация формы изменения данных пользователя

--- src/pages/profile/profile-change/profile-change.ts
+++ src/pages/profile/profile-change/profile-change.ts
@@ -1,18 +1,72 @@
+import {triggerValidateError} from "../../../utils/FormValidation";
+
 const profileForm: HTMLFormElement | null = document.querySelector('#profile_form');
+
+function validate() {
+    let result = true;
+
+    // select inputs
+    const login = profileForm?.login;
+    const firstName = profileForm?.firstName;
+    const secondName = profileForm?.secondName;
+    const mail = profileForm?.mail;
+    const phone = profileForm?.phone;
+    const displayName = profileForm?.displayName;
+
+    // hide clues
+    document.querySelector('#login-empty')?.classList.remove('show-error');
+    document.querySelector('#first-name-empty')?.classList.remove('show-error');
+    document.querySelector('#second-name-empty')?.classList.remove('show-error');
+    document.querySelector('#mail-empty')?.classList.remove('show-error');
+    document.querySelector('#phone-empty')?.classList.remove('show-error');
+    document.querySelector('#display-name-empty')?.classList.remove('show-error');
+
+    if (login.value === '') {
+        triggerValidateError(login, '#login-empty');
+        result = false;
+    }
+
+    if (firstName.value === '') {
+        triggerValidateError(firstName, '#first-name-empty');
+        result = false;
+    }
+
+    if (secondName.value === '') {
+        triggerValidateError(secondName, '#second-name-empty');
+        result = false;
+    }
+
+    if (mail.value === '') {
+        triggerValidateError(mail, '#mail-empty');
+        result = false;
+    }
+
+    if (phone.value === '') {
+        triggerValidateError(phone, '#phone-empty');
+        result = false;
+    }
+
+    if (displayName.value === '') {
+        triggerValidateError(displayName, '#display-name-empty');
+        result = false;
+    }
+
+    return result;
+}
 
 if (profileForm) {
     profileForm.onsubmit = (e) => {
+        e.preventDefault();
+
         const formData = new FormData(profileForm);
 
-        const mail = formData.get('email');
+        const mail = formData.get('mail');
         const login = formData.get('login');
-        const firstName = formData.get('first_name');
-        const secondName = formData.get('second_name');
-        const displayName = formData.get('display_name');
+        const firstName = formData.get('firstName');
+        const secondName = formData.get('secondName');
+        const displayName = formData.get('displayName');
         const phone = formData.get('phone');
 
-        console.log({ mail, login, firstName, secondName, displayName, phone });
-
-        e.preventDefault();
+        validate() ? console.log({ mail, login, firstName, secondName, displayName, phone }) : null;
     };
 }
