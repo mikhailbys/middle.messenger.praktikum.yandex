116aca31f9dd25896a778573d3088e8237ad55e2
mikhailbys <bystrovmikhailad@gmail.com>
2021-11-23 01:09:45 +0400

    ура установил моку

---
+++ src/test/index.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+
+const hello = (str: string) => 'Hello mocha';
+
+describe("Typescript + Babel usage suite", () => {
+    it("should return string correctly", () => {
+        expect(hello("mocha"), "Hello mocha");
+    });
+});
