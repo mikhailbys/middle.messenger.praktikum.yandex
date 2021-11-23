e93af1e845c675f065d36122145780480fac7a50
mikhailbys <bystrovmikhailad@gmail.com>
2021-11-24 01:07:42 +0400

    тест компонента

--- src/modules/router/router.spec.ts
+++ src/modules/router/router.spec.ts
@@ -17,15 +17,15 @@ describe("Router usage suite", () => {
         const router = new Router('app');
         assert.exists(router);
     });
+    it('Router instance has no routes', function () {
+        const router = new Router('app');
+        assert.isArray(router.routes, 'Routes is an array');
+        assert.lengthOf(router.routes, 0, 'There is no routes');
+    });
     it('Router route adding', function () {
         const router = new Router('app');
         router.use('/err', () => new Block('div'));
         assert.lengthOf(router.routes, 1, 'Router get one route');
         assert.equal(router.routes[0]._pathname, '/err', 'Route pathname is not defined');
-    });
-    it('Router instance has no routes', function () {
-        const router = new Router('app');
-        assert.isArray(router.routes, 'Routes is an array');
-        assert.lengthOf(router.routes, 0, 'There is no routes');
     });
 });
